cce50337ec33607a2674b654bddcf3003912d170
Sara Ersson <saraers@kth.se>
2019-01-23 14:57:19 +0100

    add Calculator class

---
+++ src/main/java/main/Calculator.java
@@ -0,0 +1,15 @@
+package main;
+
+
+// add different functions for calculating stuff which is needed for the different condition checks
+
+public class Calculator {
+
+    // calc radius
+
+    // calc angle
+
+    // calc area
+
+
+}
