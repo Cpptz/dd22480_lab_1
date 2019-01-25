575873975fa882ef08cceefa91374f7e6f87bc10
Robin Gunning <ro.gunning@gmail.com>
2019-01-25 10:42:30 +0100

    feat: lic#5 and unittests for lic#5

--- src/test/java/main/LICTest.java
+++ src/test/java/main/LICTest.java
@@ -17,4 +17,20 @@ class LICTest {
 
         assertEquals(LIC.LIC_0(points,3),true);
     }
+
+    @Test
+    void LIC_5() {
+        Point a = new Point(0,0);
+        Point b = new Point(0,3);
+        Point c = new Point(3,3);
+        Point d = new Point(4,3);
+        Point e = new Point(2,3);
+        Point f = new Point(0,0);
+
+        Point[] points = {a,b,c,d,e,f};
+
+        assertFalse(LIC.LIC_5(points,3));
+
+        assertTrue(LIC.LIC_5(points,6));
+    }
 }
