5fc7e949cd8c0c717831b3eec08c117344692d88
Cyril Pottiez <cyril26pot@hotmail.fr>
2019-01-28 10:00:53 +0100

    fix: return radius in all case + fix corresponding unit tests fix #51

--- src/test/java/main/CalculatorTest.java
+++ src/test/java/main/CalculatorTest.java
@@ -56,18 +56,18 @@ class CalculatorTest {
         Point b = new Point(2, 4);
         Point c = new Point(4, 8);
 
-        assertEquals(Calculator.computeRadiusTriInCircleFromPoints(a,b,c), Math.sqrt(45),DELTA);
+        assertEquals(Calculator.computeRadiusTriInCircleFromPoints(a,b,c), Math.sqrt(45)/2,DELTA);
 
         a = new Point(0, 2);
         b = new Point(-2, 0);
         c = new Point(2, 0);
-        assertEquals(Calculator.computeRadiusTriInCircleFromPoints(a,b,c), 2.0f, DELTA);
+        assertEquals(Calculator.computeRadiusTriInCircleFromPoints(a,b,c), 2.0, DELTA);
 
         a = new Point(-5, 0);
         b = new Point(0, 0.75);
         c = new Point(5, 0);
 
-        assertEquals(Calculator.computeRadiusTriInCircleFromPoints(a,b,c), 10.0, DELTA);
+        assertEquals(Calculator.computeRadiusTriInCircleFromPoints(a,b,c), 5.0, DELTA);
     }
 
     @Test
